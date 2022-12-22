e09fccac8217857c7782903ce9193ea5e63d4ddc
Harut <43827108+HarutPetrosyan@users.noreply.github.com>
2022-12-22 11:51:53 +0400

    create CatalogAPI Service with MongoDb

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,6 +2,7 @@ using Catalog.API.Data;
 using Catalog.API.Data.Inteface;
 using Catalog.API.Repositories;
 using Catalog.API.Repositories.Interface;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +12,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddScoped<ICatalogContext, CatalogContext>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>(); 
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",new OpenApiInfo{Title = "Catalog.API",Version = "v1"});
+});
 
 var app = builder.Build();
 
@@ -22,7 +26,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c=>c.SwaggerEndpoint("/swagger/v1/swagger.json","Catalog.API v1"));
 }
 
 app.UseAuthorization();
