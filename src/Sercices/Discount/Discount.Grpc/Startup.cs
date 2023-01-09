bc2b762fe1fe03461770f0610dcf04eb8939d514
Harut <43827108+HarutPetrosyan@users.noreply.github.com>
2023-01-09 09:46:35 +0400

    Discount Finish

---
+++ src/Sercices/Discount/Discount.Grpc/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Discount.API.Repositories;
+using Discount.API.Repositories.Interfacies;
+
+namespace Discount.Grpc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
+            services.AddGrpc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+               
+            }
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with Grpc endpoints must be made thrw");
+                });
+            });
+        }
+    }
+}
