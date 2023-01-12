c1ade34edc4b78d05cf52ed592a1d79d032a7d7a
Harut <43827108+HarutPetrosyan@users.noreply.github.com>
2023-01-12 17:02:35 +0400

    RabbitMQ start develop

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,7 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
-using MassTransit;
+using MassTransit; 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -38,6 +38,14 @@ namespace Basket.API
                 (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
 
             services.AddScoped<DiscountGrpcService>();
+
+            services.AddMassTransit(config =>
+            {
+                config.UsingRabbitMq((ctx, cfg) =>
+                {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
